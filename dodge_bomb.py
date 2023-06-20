272d9ce7f40281309e1efd33280623dfa892fa68
tama <c0b2205825@edu.teu.ac.jp>
2023-06-20 15:26:38 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,15 @@ delta = {
     pg.K_RIGHT: (+5, 0),
 }
 
+def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate= True, True
+    if rect.left < 0 or WIDTH < rect.right:
+        yoko=False
+    if rect.top < 0 or HEIGHT < rect.bottom:
+        tate=False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -42,11 +51,17 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         screen.blit(bg_img, [0, 0])
-        
         screen.blit(kk_img, kk_rct)
         bom_rct.move_ip(vx,vy)
+        yoko, tate=check_bound(bom_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bom,bom_rct)
         pg.display.update()
         tmr += 1
