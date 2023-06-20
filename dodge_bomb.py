ee0c0771f4e18428a86c6030451371879bb2edf5
tama <c0b2205825@edu.teu.ac.jp>
2023-06-20 14:51:05 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,12 @@ import sys
 import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {
+    pg.K_UP: (0,-5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -11,26 +16,38 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    clock = pg.time.Clock()
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=900,400
     bom=pg.Surface((20,20))
-    pg.draw.circle(bom,(255,0,0),(10,10),10)
     bom.set_colorkey((0,0,0))
+    pg.draw.circle(bom,(255,0,0),(10,10),10)
     x=random.randint(0,WIDTH)
     y=random.randint(0,WIDTH)
     bom_rct=bom.get_rect()
-    bom_rct.center=x,y #爆弾rctの中心座標を乱数で指定する
-    tmr = 0
+    bom_rct.center=x,y#爆弾rctの中心座標を乱数で指定する
     vx,vy=+5,+5
+    clock = pg.time.Clock()
+    tmr = 0
+
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst=pg.key.get_pressed()
+        sum_mv=[0,0]
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        screen.blit(bom,bom_rct)
+        
+        screen.blit(kk_img, kk_rct)
         bom_rct.move_ip(vx,vy)
+        screen.blit(bom,bom_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
